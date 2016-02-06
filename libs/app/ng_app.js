9815de87832215b50e60bfe52cfbfddb4dd11f1a
Arun R <arunr@acube.co>
2016-02-06 16:00:14 +0530

    angular template integration done

--- libs/app/ng_app.js
+++ libs/app/ng_app.js
@@ -1,1 +1,25 @@
-var app = angular.module('bootstrapApp',[]);
+
+var app = angular.module('bootstrapApp', ['ngRoute']);
+
+
+	app.config(['$routeProvider', function($routeProvider,loggedIn,$timeout) {
+		$routeProvider.
+		when('/home', {templateUrl: 'pages/home.html', controller: 'HomeCtrl'}).
+		when('/About', {templateUrl: 'pages/aboutus.html', controller: 'AboutusCtrl'}).
+		when('/Contact', {templateUrl: 'pages/contactus.html', controller: 'ContactusCtrl'}).
+	    otherwise({redirectTo: '/home'});
+	}]);	
+	app.controller('HomeCtrl',function ($scope) {
+	    $scope.page_title = "About Us";
+		$scope.page_slogan = "Why our Clients love to work with us.";
+	});
+	app.controller('AboutusCtrl',function ($scope) {
+	    $scope.page_title = "About Us";
+		$scope.page_slogan = "Why our Clients love to work with us.";
+	});	
+	app.controller('ContactusCtrl',function ($scope) {
+	    $scope.page_title = "Contact Us";
+		$scope.page_slogan = "Stay close1.";
+	});
+
+	
